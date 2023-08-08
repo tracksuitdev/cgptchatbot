e84357d41e7451fed34c9ab705bc238ef6e55ad5
tracksuitdev <81776803+tracksuitdev@users.noreply.github.com>
2023-08-09 00:23:45 +0200

    basic ui

--- src/cgptchatbotapp.c
+++ src/cgptchatbotapp.c
@@ -20,7 +20,7 @@ static void cgpt_chatbot_app_activate(GApplication *app) {
 }
 
 static void cgpt_chatbot_app_open(GApplication *app, GFile **files, gint n_files, const gchar *hint) {
-    CgptChatbotAppWindow *win = cgpt_chatbot_app_window_new(CGPTCHATBOT_APP(app));
+    CgptChatbotAppWindow *win;
     GList *windows = gtk_application_get_windows(GTK_APPLICATION(app));
     if (windows) {
         win = CGPTCHATBOT_APP_WINDOW(windows->data);
@@ -33,16 +33,38 @@ static void cgpt_chatbot_app_open(GApplication *app, GFile **files, gint n_files
     gtk_window_present(GTK_WINDOW(win));
 }
 
+static void preferences_activated(GSimpleAction *action, GVariant *parameter, gpointer app) {
+    g_print("Preferences activated\n");
+}
+
+static void quit_activated(GSimpleAction *action, GVariant *parameter, gpointer app) {
+    g_application_quit(G_APPLICATION(app));
+}
+
+static GActionEntry app_entries[] = {
+        {"preferences", preferences_activated, NULL, NULL, NULL},
+        {"quit",        quit_activated,        NULL, NULL, NULL}
+};
+
+static void cgpt_chatbot_app_startup(GApplication *app) {
+    G_APPLICATION_CLASS(cgpt_chatbot_app_parent_class)->startup(app);
+    gtk_application_set_accels_for_action(GTK_APPLICATION(app), "app.quit", (const gchar *[]){"<Ctrl>Q", NULL});
+    g_action_map_add_action_entries(G_ACTION_MAP(app), app_entries, G_N_ELEMENTS(app_entries), app);
+}
+
+
 static void cgpt_chatbot_app_class_init(CgptChatbotAppClass *class) {
+    G_APPLICATION_CLASS(class)->startup = cgpt_chatbot_app_startup;
     G_APPLICATION_CLASS(class)->activate = cgpt_chatbot_app_activate;
     G_APPLICATION_CLASS(class)->open = cgpt_chatbot_app_open;
 }
 
 CgptChatbotApp *cgptchatbot_app_new(void) {
     return g_object_new(CGPTCHATBOT_APP_TYPE,
-                        "application-id", "org.gtk.cgptchatbot",
+                        "application-id", "org.tracksuitdev.cgptchatbot",
                         "flags", G_APPLICATION_HANDLES_OPEN,
                         NULL);
 }
+
 
 
--- src/cgptchatbotwin.c
+++ src/cgptchatbotwin.c
@@ -1,1 +1,27 @@
 #include "cgptchatbotwin.h"
+
+struct _CgptChatbotAppWindow {
+    GtkApplicationWindow parent;
+    GtkStack *stack;
+};
+
+G_DEFINE_TYPE(CgptChatbotAppWindow, cgpt_chatbot_app_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void cgpt_chatbot_app_window_init(CgptChatbotAppWindow *win) {
+    gtk_widget_init_template(GTK_WIDGET(win));
+}
+
+static void cgpt_chatbot_app_window_class_init(CgptChatbotAppWindowClass *class) {
+    gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/org/tracksuitdev/cgptchatbot/window.ui");
+    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class), CgptChatbotAppWindow, stack);
+}
+
+CgptChatbotAppWindow *cgpt_chatbot_app_window_new(CgptChatbotApp *app) {
+    return g_object_new(CGPT_CHATBOT_APP_WINDOW_TYPE,
+                        "application", app,
+                        NULL);
+}
+
+void cgpt_chatbot_app_window_open(CgptChatbotAppWindow *win, GFile *file) {
+
+}
